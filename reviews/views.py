49c7ab9a26c910514f4f190336ac8716ebd3f0ff
LiquidIce64 <gilyazov.rm2005@gmail.com>
2023-10-11 17:52:59 +0300

    Added search results page

--- reviews/views.py
+++ reviews/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     name = request.GET.get("name") or "world"
     return render(request, "base.html", {"name": name})
+
+
+def search_result(request):
+    search_words = request.GET.get("search") or ""
+    return render(request, "search_result.html", {"search_words": search_words})
