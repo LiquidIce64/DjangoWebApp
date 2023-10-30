526070b60f414b54c2424ddb790258cd6b7cf0bf
LiquidIce64 <gilyazov.rm2005@gmail.com>
2023-10-30 15:41:23 +0300

    Added book details page

--- reviews/utils.py
+++ reviews/utils.py
@@ -1,3 +1,18 @@
 def average_rating(rating_list):
     if not rating_list: return 0
     return round(sum(rating_list) / len(rating_list))
+
+
+def get_book_details(book):
+    reviews = book.review_set.all()
+    if reviews:
+        book_rating = average_rating([review.rating for review in reviews])
+        number_of_reviews = len(reviews)
+    else:
+        book_rating = None
+        number_of_reviews = 0
+    return {
+        'book': book,
+        'rating': book_rating,
+        'number_of_reviews': number_of_reviews
+    }
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Book
-from .utils import average_rating
+from .utils import average_rating, get_book_details
 
 
 def index(request):
@@ -17,20 +17,17 @@ def search_result(request):
 
 def book_list(request):
     books = Book.objects.all()
-    book_list = []
-    for book in books:
-        reviews = book.review_set.all()
-        if reviews:
-            book_rating = average_rating([review.rating for review in reviews])
-            number_of_reviews = len(reviews)
-        else:
-            book_rating = None
-            number_of_reviews = 0
-        book_list.append({'book': book,
-                          'book_rating': book_rating,
-                          'number_of_reviews': number_of_reviews})
-
     context = {
-        'book_list': book_list
+        'book_list': [get_book_details(book) for book in books]
     }
     return render(request, 'reviews/book_list.html', context)
+
+
+def book_details(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    reviews = book.review_set.all()
+    context = {
+        'book_details': get_book_details(book),
+        'reviews': reviews
+    }
+    return render(request, 'reviews/book_details.html', context)
