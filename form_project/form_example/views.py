ffecd2b186eb7bb1dfe04d48699f0fb3b7ecb919
LiquidIce64 <gilyazov.rm2005@gmail.com>
2023-12-10 17:03:10 +0300

    Django forms

--- form_project/form_example/views.py
+++ form_project/form_example/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .forms import ExampleForm
 
-# Create your views here.
+
+def form_example(request):
+    for name in request.POST:
+        print(f"{name}: {request.POST.getlist(name)}")
+    return render(request, 'form-example.html', {
+        "method": request.method,
+        "form": ExampleForm()
+    })
--- form_project/form_project/urls.py
+++ form_project/form_project/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+from form_example import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name="form-example.html")),
+    path("", views.form_example),
 ]
